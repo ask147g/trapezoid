7c5e33633b340b6d73526fff76293888785bf4a1
veoutlo <veoutlo@gmail.com>
2023-06-05 14:15:55 +0700

    added files to edit

--- src/consts/check.jsx
+++ src/consts/check.jsx
@@ -1,6 +1,8 @@
 import {area} from "./sizes"
 export function CheckResult() {
     // Найти input и прочитать значение
+    // P.S. получить элемент html по имени (из свойств name) const result = document.getElementsByName("result"); 
+    // получить значение введенное пользователем let res = result[0].value;
     // т.к. переменная с input является string, для корректности перевести в float
     // сравнить введенное пользователем значение с правильным
     // возвращает true или false
